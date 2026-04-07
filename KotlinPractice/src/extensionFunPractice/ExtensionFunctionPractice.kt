1808b5da84090ff43898c4077a6765ea2d44d452
Md. AL MUHEETU <almuheetu@gmail.com>
2026-04-07 12:11:25 +0600

    Add Int.isEven extension and usage
    Introduce an Int.isEven() extension that returns true for even numbers. Update main() to declare an age variable (24) and demonstrate the new extension by printing its result along with a combined "Name: <name>, Age: <age>" line.

--- KotlinPractice/src/extensionFunPractice/ExtensionFunctionPractice.kt
+++ KotlinPractice/src/extensionFunPractice/ExtensionFunctionPractice.kt
@@ -1,10 +1,25 @@
 package extensionFunPractice
 
 // Extension Function
+// Extension Function কেন ব্যবহার করি?
+// Class modify না করে clean way তে feature add করা যায়
+
+// [ধরো, একটা class আছে — তুমি সেটা change করতে পারছো না (library class বা built-in),
+// কিন্তু তুমি চাইছো ওই class-এর উপর নতুন একটা function ব্যবহার করতে। তখন তুমি extension function ব্যবহার করো।]
+
+
 fun String.addWelcome(): String = "Welcome to $this"
 
+fun Int.isEven(): Boolean {
+    return this % 2 == 0
+}
 
 fun main() {
     val name = "Shihab"
+    val age = 24
+
+
     println(name.addWelcome())
+    println(age.isEven())
+    println("Name: $name, Age: $age")
 }
