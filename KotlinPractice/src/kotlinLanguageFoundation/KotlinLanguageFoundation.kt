39fcd46af09f7fb3ff5802c522bc33f731da4e96
MD. AL MUMEETU <almuheetu@gmail.com>
2026-05-11 00:43:26 +0600

    Operator & ControlFLow

--- KotlinPractice/src/kotlinLanguageFoundation/KotlinLanguageFoundation.kt
+++ KotlinPractice/src/kotlinLanguageFoundation/KotlinLanguageFoundation.kt
@@ -3,6 +3,10 @@ package kotlinLanguageFoundation
 val name = "Shihab"
 val experiance = 3
 
+
+//Operator & ControlFLow
+val cartTotal = 1500.0
+val hasVipCard = true
 fun main() {
     val birthYear = 2000
     var currentAge = 24
@@ -15,4 +19,7 @@ fun main() {
     println("I work at ${company.uppercase()} and my age is $currentAge")
     println("price : $price")
     println("intPrice : $intPrice")
+    //Operator & ControlFLow
+    val isEligibleForDiscount = (cartTotal > 1000.0) && hasVipCard
+    println("Discount Eligible for : $isEligibleForDiscount")
 }
