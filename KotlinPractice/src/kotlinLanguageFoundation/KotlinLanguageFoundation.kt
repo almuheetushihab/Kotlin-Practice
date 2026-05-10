cdd14c5f0b4e8c5ed3a0b3c3be8061a61576d891
MD. AL MUMEETU <almuheetu@gmail.com>
2026-05-11 00:51:03 +0600

    ControlFLow

--- KotlinPractice/src/kotlinLanguageFoundation/KotlinLanguageFoundation.kt
+++ KotlinPractice/src/kotlinLanguageFoundation/KotlinLanguageFoundation.kt
@@ -4,9 +4,7 @@ val name = "Shihab"
 val experiance = 3
 
 
-//Operator & ControlFLow
-val cartTotal = 1500.0
-val hasVipCard = true
+
 fun main() {
     val birthYear = 2000
     var currentAge = 24
@@ -15,11 +13,20 @@ fun main() {
     val price: Double = 99.99
     val intPrice: Int = price.toInt()
     val company: String = "Softzino"
+    //Operator & ControlFLow
+    val cartTotal = 1500.0
+    val hasVipCard = true
+
+    val stock = 1
     println("I am $name and I have $experiance years of experience, and my birth year is $birthYear")
     println("I work at ${company.uppercase()} and my age is $currentAge")
     println("price : $price")
     println("intPrice : $intPrice")
+
+
     //Operator & ControlFLow
     val isEligibleForDiscount = (cartTotal > 1000.0) && hasVipCard
     println("Discount Eligible for : $isEligibleForDiscount")
+    val stockStatus = if (stock > 0) "In Stock" else "Out of Stock"
+    println(stockStatus)
 }
