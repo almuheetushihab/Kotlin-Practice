4de4eecd270d01b4e0205d54cb9c0951aabe57a2
MD. AL MUMEETU <almuheetu@gmail.com>
2026-05-11 00:24:57 +0600

    Invalid configuration: Token is required for the selected model..

--- KotlinPractice/src/kotlinLanguageFoundation/KotlinLanguageFoundation.kt
+++ KotlinPractice/src/kotlinLanguageFoundation/KotlinLanguageFoundation.kt
@@ -4,5 +4,15 @@ val name = "Shihab"
 val experiance = 3
 
 fun main() {
-    println("I am $name and I have $experiance years of experience")
+    val birthYear = 2000
+    var currentAge = 24
+    currentAge = 26
+
+    val price: Double = 99.99
+    val intPrice: Int = price.toInt()
+    val company: String = "Softzino"
+    println("I am $name and I have $experiance years of experience, and my birth year is $birthYear")
+    println("I work at ${company.uppercase()} and my age is $currentAge")
+    println("price : $price")
+    println("intPrice : $intPrice")
 }
